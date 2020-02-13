9bd80fdaf87bd59f63b8d60adb935f13f8754a07
shajun <junco.sha@gmail.com>
2020-02-13 17:30:20 +0800

    feat: Add start files for client and server folder

--- src/server/server.js
+++ src/server/server.js
@@ -1,31 +1,39 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
-// Require Express to run server and routes
-const express = require('express')
+var json = {
+  title: 'test json response',
+  message: 'this is a message',
+  time: 'now'
+};
 
-// Start up an instance of app
-const app = express()
-
-/* Middleware*/
-const bodyParser = require('body-parser');
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-const cors = require('cors');
+const app = express();
 app.use(cors());
+// to use json
+app.use(bodyParser.json());
+// to use url encoded values
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-// Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('dist'));
 
+console.log(JSON.stringify(mockAPIResponse));
 
-// Setup Server
-const port = 8000;
-const server = app.listen(port, listening);
+app.get('/', function(req, res) {
+  res.sendFile('dist/index.html');
+});
 
-function listening() {
-  console.log('Server is running!');
-  console.log(`running on localhost: ${port}`);
-}
+app.get('/test', function(req, res) {
+  res.json(mockAPIResponse);
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function() {
+  console.log('Example app listening on port 8081!');
+});
